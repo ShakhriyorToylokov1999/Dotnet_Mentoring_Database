6be6adfcb69a9c657095e2481c66919162a49303
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-08-19 19:34:26 +0500

    Add project files.

---
+++ Address.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Street] NCHAR(50) NOT NULL,
+	[City] NCHAR(20) NULL,
+	[State] NCHAR(50) NULL,
+	[ZipCode] NCHAR(50) NULL
+)
---
+++ Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NULL, 
+    [LastName] NCHAR(50) NULL
+    
+)
