e75d6b6af460f9b20f660c763a55d89cf256797f
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-08-19 20:31:51 +0500

    recommit

---
+++ EmployeeTrigger.sql
@@ -0,0 +1,28 @@
+﻿CREATE TRIGGER CreateCompanyOnEmployeeInsert
+ON Employee
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @AddressId int;
+    DECLARE @CompanyId int;
+
+    SELECT @AddressId = i.AddressId
+    FROM inserted i;
+
+    INSERT INTO Address (Street, City, State, ZipCode)
+    SELECT Street, City, State, ZipCode
+    FROM Address
+    WHERE Id = @AddressId;
+
+    SET @AddressId = SCOPE_IDENTITY();
+
+    SELECT @CompanyId = i.CompanyId
+    FROM inserted i;
+
+    INSERT INTO Company (Name, AddressId)
+    SELECT CompanyName, @AddressId
+    FROM Employee
+    WHERE Id = @CompanyId;
+END
