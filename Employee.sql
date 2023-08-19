8808f633a258950fb4da680eb72b2bbf526bcdf3
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-08-19 19:47:31 +0500

    Task 2 completed

--- Employee.sql
+++ Employee.sql
@@ -3,8 +3,10 @@
 	[Id] INT NOT NULL PRIMARY KEY,
 	[AddressId] INT NOT NULL,
 	[PersonId] INT NOT NULL,
+	[CompanyName] NVARCHAR(100) NOT NULL,
 	[Position] NVARCHAR(30) NULL,
 	[EmployeeName] NVARCHAR(100) NULL,
 	CONSTRAINT FK_Employee_Person FOREIGN KEY (PersonId) REFERENCES Person(Id),
-	CONSTRAINT FK_Employee_Address FOREIGN KEY (AddressId) REFERENCES Address(Id)
+	CONSTRAINT FK_Employee_Address FOREIGN KEY (AddressId) REFERENCES Address(Id),
+	CONSTRAINT FK_Employee_Company FOREIGN KEY (CompanyName) REFERENCES Company(Name)
 );
---
+++ EmployeeInfo.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW EmployeeInfo AS
+SELECT e.Id AS EmployeeId,
+       COALESCE(e.EmployeeName, p.FirstName + ' ' + p.LastName) AS EmployeeFullName,
+       a.ZipCode + '_' + COALESCE(a.State, '') + ', ' + COALESCE(a.City, '') + '-' + COALESCE(a.Street, '') AS EmployeeFullAddress,
+       c.Name + '(' + COALESCE(e.Position, '') + ')' AS EmployeeCompanyInfo,
+       ROW_NUMBER() OVER (ORDER BY c.Name ASC, COALESCE(a.City, '') ASC) AS RowNum
+FROM Employee e
+JOIN Person p ON e.PersonId = p.Id
+JOIN Address a ON e.AddressId = a.Id
+JOIN Company c ON e.CompanyName = c.Name;
+
+GO
+
+CREATE VIEW EmployeeInfoSorted AS
+SELECT EmployeeId, EmployeeFullName, EmployeeFullAddress, EmployeeCompanyInfo
+FROM EmployeeInfo
+ORDER BY RowNum;
+
+GO
+
+DROP VIEW EmployeeInfo;
